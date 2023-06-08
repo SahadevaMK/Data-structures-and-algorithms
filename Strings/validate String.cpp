91c2f644b6534fdbbd7eaa1fd1c6fb996d3b2ab0
Sahadeva Gowda <sahadevamk121@gmail.com>
2023-06-09 00:27:45 +0530

    Create validate String.cpp

---
+++ Strings/validate String.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+  char s[] = "how rre you";
+  int i,words;
+  int c = 1;
+  int v = 2;
+  for (int i=0;s[i]!='\0';i++){
+      if (!(s[i]>=65 && s[i] <=90) && !(s[i]>=97 && s[i] <=122) && !(s[i]>=48 && s[i] <=57)){
+          return c;
+      }
+  }
+  return v;
+}
+validate a string means a string should have only upper and lower case alphabets and numbers in it no special characters
