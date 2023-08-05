ce14654edd7303fae904edec658b8131ef8876f7
Sahadeva Gowda <sahadevamk121@gmail.com>
2023-08-05 15:41:18 +0530

    Update optimizedBubbleSort.py

--- Sorting Algorithms/optimizedBubbleSort.py
+++ Sorting Algorithms/optimizedBubbleSort.py
@@ -3,10 +3,10 @@ arr = [1,3,6,9,5,4,2,3]
 def bubbleSort(customArr):
     for i in range(len(customArr)-1):
         flag = 0
-        for j in range(len(customArr)-1):
+        for j in range(len(customArr)-1-i):
             if customArr[j] > customArr[j+1]:
                 customArr[j] , customArr[j+1]  = customArr[j+1] , customArr[j]
-                flaf = 1
+                flag = 1
         if flag == 0:
             break
 
